c648efff50276b141791cbfa72343ccc19733b33
Andrea Riciputi <andrea.riciputi@gmail.com>
2015-04-16 18:23:32 +0200

    Better color support in ls

--- aliases.zsh
+++ aliases.zsh
@@ -1,19 +1,30 @@
-alias l='CLICOLOR_FORCE=1 ls -G'
-alias ll='CLICOLOR_FORCE=1 ls -GFlh'
-alias l.='CLICOLOR_FORCE=1 ls -GA'
-alias ll.='CLICOLOR_FORCE=1 ls -GFalh'
-alias lsd='CLICOLOR_FORCE=1 ls -ld *(-/DN)' ## list only dirs
-alias lsh='CLICOLOR_FORCE=1 ls -d .*' # list only dot files
-alias llh='CLICOLOR_FORCE=1 ls -ld .*' # list only dot files (long form)
+if whichplatform 'darwin'; then
+    alias l='ls -G'
+    alias ll='ls -GFlh'
+    alias l.='ls -GA'
+    alias ll.='ls -GFalh'
+    alias lsd='ls -ld *(-/DN)' ## list only dirs
+    alias lsh='ls -d .*' # list only dot files
+    alias llh='ls -ld .*' # list only dot files (long form)
 
-alias du1='du -chs *(/)' ## du with depth 1
+elif whichplatform 'linux'; then
+    alias l='ls --color=auto -G'
+    alias ll='ls --color=auto -GFlh'
+    alias l.='ls --color=auto -GA'
+    alias ll.='ls --color=auto -GFalh'
+    alias lsd='ls --color=auto -ld *(-/DN)' ## list only dirs
+    alias lsh='ls --color=auto -d .*' # list only dot files
+    alias llh='ls --color=auto -ld .*' # list only dot files (long form)
+fi
 
-alias d='dirs -v'
+    alias du1='du -chs *(/)' ## du with depth 1
 
-alias cls='clear'
+    alias d='dirs -v'
 
-alias lv='/usr/local/share/vim/vim74/macros/less.sh'
-alias vr='vim -R'
+    alias cls='clear'
 
-alias -g ..='cd ..'
-alias -g ...='cd ../..'
+    alias lv='/usr/local/share/vim/vim74/macros/less.sh'
+    alias vr='vim -R'
+
+    alias -g ..='cd ..'
+    alias -g ...='cd ../..'
--- colors.zsh
+++ colors.zsh
@@ -5,8 +5,12 @@ if [[ "$terminfo[colors]" -ge 8 ]]; then
 
     ## ---
     ## Colorize ls (search man ls for LSCOLORS)
-    export CLICOLOR=1
-    export LSCOLORS=cxfxexdxbxehbhgefehchd
+    if whichplatform 'darwin'; then
+        export CLICOLOR=1
+        export LSCOLORS=cxfxexdxbxehbhgefehchd
+    elif whichplatform 'linux'; then
+        export LS_COLORS="di=32;40:ln=35;40:so=34;40:pi=33;40:ex=31;40:bd=34;47:cd=31;47:su=36;44:sg=35;44:tw=37;42:ow=37;43:"
+    fi
     export GREP_OPTIONS='--color=auto'
 fi
 
