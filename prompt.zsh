ccc1154fa0d24d36b2c42c7de9728e1445b88641
Andrea Riciputi <andrea.riciputi@statpro.com>
2015-07-24 11:27:56 +0200

    Better prompt color and support for git/hg

--- prompt.zsh
+++ prompt.zsh
@@ -8,12 +8,12 @@ if [[ "$terminfo[colors]" -ge 8 ]]; then
     # make sure colors are loaded
     autoload -U colors && colors
 
-    PS1='%F{099}%D{%a %d %b} %F{111}%* %F{037}%n %F{039}%m ${vcs_info_msg_0_}%F{reset}
+    PS1='%F{099}%D{%a %d %b} %F{111}%* %F{044}%n %F{039}%m ${vcs_info_msg_0_}%F{reset}
 %(?..%F{009}%?%f )%F{011}%#%F{reset} '
 
-    local FMT_PATH="%F{196}%s %F{185}%r %F{208}%b %F{40}%35<..<%S%<<%F{reset}"
-    local FMT_ACTION="%F{196}%s %F{185}%r %F{208}%b %F{207}%a %F{40}%35<..<%S%<<%F{reset}"
-    local FMT_NOVCS="%F{40}%45<..<%~%<<%F{reset}"
+    local FMT_PATH="%F{100}%s %F{185}%r %F{208}%b %c%u %F{040}%35<..<%S%<<%F{reset}"
+    local FMT_ACTION="%F{100}%s %F{185}%r %F{208}%b %F{207}%a %c%u %F{040}%35<..<%S%<<%F{reset}"
+    local FMT_NOVCS="%F{040}%45<..<%~%<<%F{reset}"
 
 else
     # Set a b/w prompt
@@ -28,6 +28,12 @@ fi
 
 # Setup vcs_info
 zstyle ":vcs_info:*" enable git hg
+zstyle ":vcs_info:(hg*|git*):*" check-for-changes true
+zstyle ":vcs_info:hg*:*" use-simple false
+zstyle ":vcs_info:hg*:*" get-revision true
+zstyle ":vcs_info:hg*:*" hgrevformat ''
+zstyle ':vcs_info:(hg*|git*):*' stagedstr '%F{28}o'
+zstyle ':vcs_info:(hg*|git*):*' unstagedstr '%F{196}x'
 zstyle ":vcs_info:(hg*|git*):*" formats       "${FMT_PATH}"   ""
 zstyle ":vcs_info:(hg*|git*):*" actionformats "${FMT_ACTION}" ""
 zstyle ":vcs_info:*" nvcsformats   "${FMT_NOVCS}"  ""
